e9aea06f26650cc9087ebd879a2af99553df36f8
Jevdev <jev.dev.art@gmail.com>
2026-05-19 18:00:13 +0200

    Update v.4.3
    Collectables FIXED, AudioMixer added, ring collecting animation addition attempted (failed)

--- Assets/Scripts/RingTrigger.cs
+++ Assets/Scripts/RingTrigger.cs
@@ -1,20 +1,36 @@
+using System.Collections;
 using UnityEngine;
 
 public class RingTrigger : MonoBehaviour
 {
-    AudioSource m_AudioSource;
+    private AudioSource m_AudioSource;
+    private Animator animator;
+    private Collider2D objectCollider;
+    private SpriteRenderer spriteRenderer;
+
     private void Start()
     {
-        m_AudioSource = GetComponrnt<AudioSource>();
+        m_AudioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+        objectCollider = GetComponent<Collider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
-       private void OnTriggerEnter2D(Collider2D collision)
-       {
-           if (collision.CompareTag("Player"))
-           {
-               m_AudioSource.Play();
-               Delay(0.5f);
-               Destroy(gameObject);
-            }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            objectCollider.enabled = false;
+            m_AudioSource.Play();
+
+            StartCoroutine(Delay());
         }
+    }
+
+    private IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(0.9f);
+        animator.Play("ring collected");
+        Destroy(gameObject);
     }
 }
